9ccc13abe9e1b96c0cca84ca2fe4853745e0766b
naydyonov <naydyonov@etm-system.ru>
2020-09-05 19:46:21 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { UsersListComponent } from './users-list/users-list.component';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,6 +23,10 @@ import { UsersListComponent } from './users-list/users-list.component';
     BrowserAnimationsModule,
     MatListModule,
     MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../users.service';
+import {User} from '../shared/user';
+import {MatListOption} from '@angular/material/list';
 
 @Component({
   selector: 'app-users-list',
@@ -7,106 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  constructor() { }
+  users: User [] = [];
+  username: string;
+  name: string;
+  role: string;
+  selectedList: User[];
 
-  usersList = [
-    {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
-      role: 'Admin',
-      phone: '1-770-736-8031 x56442',
-      website: 'hildegard.org'
-    },
-    {
-      id: 2,
-      name: 'Ervin Howell',
-      username: 'Antonette',
-      email: 'Shanna@melissa.tv',
-      role: 'Admin',
-      phone: '010-692-6593 x09125',
-      website: 'anastasia.net'
-    },
-    {
-      id: 3,
-      name: 'Clementine Bauch',
-      username: 'Samantha',
-      email: 'Nathan@yesenia.net',
-      role: 'User',
-      phone: '1-463-123-4447',
-      website: 'ramiro.info'
-    },
-    {
-      id: 4,
-      name: 'Patricia Lebsack',
-      username: 'Karianne',
-      email: 'Julianne.OConner@kory.org',
-      role: 'User',
-      phone: '493-170-9623 x1567',
-      website: 'kale.biz'
-    },
-
-    {
-      id: 5,
-      name: 'Chelsey Dietrich',
-      username: 'Kamren',
-      email: 'Lucio_Hettinger@annie.ca',
-      role: 'User',
-      phone: '(254)954-1289',
-      website: 'demarco.info'
-
-    },
-    {
-      id: 6,
-      name: 'Mrs. Dennis Schulist',
-      username: 'Leopoldo_Corkery',
-      email: 'Karley_Dach@jasper.info',
-      role: 'Admin',
-      phone: '1-477-935-8478 x6430',
-      website: 'ola.org'
-
-    },
-    {
-      id: 7,
-      name: 'Kurtis Weissnat',
-      username: 'Elwyn.Skiles',
-      email: 'Telly.Hoeger@billy.biz',
-      role: 'Admin',
-      phone: '210.067.6132',
-      website: 'elvis.io'
-
-    },
-    {
-      id: 8,
-      name: 'Nicholas Runolfsdottir V',
-      username: 'Maxime_Nienow',
-      email: 'Sherwood@rosamond.me',
-      role: 'User',
-      phone: '586.493.6943 x140',
-      website: 'jacynthe.com'
-    },
-    {
-      id: 9,
-      name: 'Glenna Reichert',
-      username: 'Delphine',
-      email: 'Chaim_McDermott@dana.io',
-      role: 'User',
-      phone: '(775)976-6794 x41206',
-      website: 'conrad.com'
-    },
-    {
-      id: 10,
-      name: 'Clementina DuBuque',
-      username: 'Moriah.Stanton',
-      email: 'Rey.Padberg@karina.biz',
-      role: 'User',
-      phone: '024-648-3804',
-      website: 'ambrose.net'
-    }
-  ];
+  constructor(public usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.users = this.usersService.usersList;
   }
 
+  search(query: string) {
+    this.users = this.usersService.findUser(query);
+  }
+
+  sort(value: string) {
+    this.users = this.usersService.sortList(value);
+  }
+
+  addUser(): any {
+    this.usersService.addUser({
+      id: Math.floor((Math.random() * 6) + 10),
+      name: this.name,
+      username: this.username,
+      email: '',
+      role: this.role,
+      phone: '',
+      website: '0'
+    });
+    this.users = this.usersService.usersList;
+    this.clearFields();
+  }
+
+  clearFields(): any {
+    this.username = this.name = '';
+    this.role = '';
+  }
+
+  deleteUsers() {
+    this.usersService.deleteUsers(this.selectedList);
+    this.users = this.usersService.usersList;
+  }
+
+  selectItem(users: MatListOption[]) {
+    this.selectedList = [];
+    users.forEach(element => {
+      this.selectedList.push(element.value);
+      console.log(element);
+    });
+  }
 }
